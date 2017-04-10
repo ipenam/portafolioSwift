bdb4af9ac0b1455a341deec25c944413d2ed9ef6
Ivan Peña <ivan.pena@bcm-corp.com>
2017-04-10 11:34:35 -0500

    WorldTrotter initial commit

---
+++ Portafolio/MapViewController.swift
@@ -0,0 +1,44 @@
+//
+//  MapViewController.swift
+//  Portafolio
+//
+//  Created by Ivan Peña on 14/03/17.
+//  Copyright © 2017 ipenam. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        // Create a map view
+        mapView = MKMapView()
+        
+        // Set it as *the* view of this view controller
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.topAnchor)
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MapViewController loaded its view.")
+    }
+    
+}
